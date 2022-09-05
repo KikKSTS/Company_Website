d26b646bd592c93b817590f6a2a4d0fcbba31823
KikKSTS <kik@yojoies.com>
2022-09-05 17:52:50 +0700

    new commit 5/9/22

--- js/navbar.js
+++ js/navbar.js
@@ -1,4 +1,4 @@
-<<<<<<<< HEAD:js/navbar.js
+
 /* section1 */
 
 let slideIndex = 1;
@@ -28,35 +28,4 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-========
-/* section1 */
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
->>>>>>>> f2cfce2f1708fc0d0954c55cde18a407544c7b17:section1.js
 }
