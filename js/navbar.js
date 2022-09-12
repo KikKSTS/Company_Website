3c68e94e6f9372cc4a89d92f1475b7421b5d9cc8
KikKSTS <kik@yojoies.com>
2022-09-12 17:54:47 +0700

    new commit 12/9/22

--- js/navbar.js
+++ js/navbar.js
@@ -1,9 +1,38 @@
-function myFunction() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+// change EN --> TH header navbar
+const btn = document.getElementById('btn');
+
+// ✅ Toggle button text on click
+btn.addEventListener('click', function handleClick() {
+  const initialText = 'EN';
+
+  if (btn.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+    btn.textContent = 'TH';
+  } else {
+    btn.textContent = initialText;
   }
-  
+});
+
+// change EN --> TH side navbar
+
+const btns = document.getElementById('btns');
+
+// ✅ Toggle button text on click
+btns.addEventListener('click', function handleClick() {
+  const initialText = 'EN';
+
+  if (btns.textContent.toLowerCase().includes(initialText.toLowerCase())) {
+    btns.textContent = 'TH';
+  } else {
+    btns.textContent = initialText;
+  }
+});
+
+// ----------------------------------------------------------------
+// sidebar
+function openNav(){
+  document.getElementById("sidenav").style.width = "40%";
+}
+function closeNav(){
+  document.getElementById("sidenav").style.width = "0%";
+}
+
